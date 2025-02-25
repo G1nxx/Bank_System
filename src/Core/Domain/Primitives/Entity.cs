bdfa190b2f662a350505f7ffdffa3acab08b2673
G1nxx <dmitrij20056@gmail.com>
2025-02-25 13:15:28 +0300

    added .gitignore, created clean-based arch, added Domain & Application libs

---
+++ src/Core/Domain/Primitives/Entity.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Domain.Primitives
+{
+    internal class Entity
+    {
+        protected Entity(Guid id) => Id = id;
+        protected Entity() {}
+        public Guid Id { get; protected set; }
+    }
+}
